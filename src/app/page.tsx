f97f0e06e8ffc746600f67ee46d278da7e2e9268
kousarshah <162299975+kousarshah@users.noreply.github.com>
2024-11-10 22:33:57 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-
-
-import Header from './components/header';
-import Footer from './components/Footer';
 import './home.css';
 import  Image from "next/image";
 import logo from './public/logo.jpg';
@@ -16,18 +12,3 @@ export default function Home() {
    </div>
   );
 }
-// export default function Home() {
-//   return (
-//     <div className="hero">
-//       <p className="para">Hi, I am</p>
-//       <h1 className="main-heading">Yusra Fatima</h1>
-//       <h2 className="h2">Front-end developer</h2>
-
-//       <Image
-//         src={girl}
-//         width={300}
-//         height={900}
-//         alt="Girl Image"
-//         className="girl"
-//       />
-//       )}
